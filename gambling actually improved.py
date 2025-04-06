6a089e23620032df7f2e29085bd48f2539214cdb
MizanZero <mizan.zafar2016@gmail.com>
2025-04-06 11:56:43 +0530

    logic works, glitch: every player is duplicated

--- gambling actually improved.py
+++ gambling actually improved.py
@@ -15,26 +15,67 @@ Scores of a player from all rounds are added to the final score.
 import random
 
 player = [None] # list of player instances (1 indexed)
+rounds = 0 # global variable for number of rounds
+r = 0 # global variable for current round
+players = 0 # global variable to keep track of number of players
 
 class Player:
     def __init__(self, name):
         self.name = name
         self.finalScore = 0
-        self.scoreInRound = [0 for i in range(globals().get('rounds'))]
+        self.scoreInRound = [None] + [0 for i in range(rounds)] # 1 indexed
         player.append(self)
 
-def shuffle(player: list, rounds: int):
+def shuffle(player: list):
     players = len(player) # number of players
     playerT = player[1:]  # temporary list of players (0th index will be None)
     random.shuffle(playerT) # shuffle the players
     player = [None]+playerT # Thank you NCR
-    for r in range(1,rounds+1):
-        for p in range(1,players+1):
-            playTurnOf(p,r)
 
-def playTurnOf(p: Player, r: int):
-    assert r in range(1,11) , "Rounds should be between 1 and 10"
-    roll = 0
-    while choice not in ['n','y']:
-        choice = input(f"Do you want to roll? y or n: ")
-        
+def playTurnOf(p: Player):
+    assert rounds in range(1,11) , "Rounds should be between 1 and 10"
+
+    while True:
+        global r
+        global player
+        global players
+
+        choice = input("Do you want to roll? y or n: ")
+        if choice.lower() == 'y':
+            throw = random.randint(1,6)
+            # score update or elimination after throw
+            if throw != 1: p.scoreInRound[r] += throw
+            else: print(f"{p.name} was eliminated after rolling a 1"); return
+
+        elif choice.lower() == 'n':
+            return
+        else:
+            print("Please enter a valid choice")
+
+def start():
+    global r
+    global rounds
+
+    for i in range(1,rounds+1):
+        r+=1
+        for p in player[1:]:
+            playTurnOf(p)
+
+rounds = int(input("Enter number of rounds: "))
+assert rounds in range(1,11), "Rounds should be between 1 and 10"
+
+players = int(input("Enter number of players: "))
+
+for i in range(1,players+1):
+    player.append(Player(input(f"Enter name of player {i}: ")))
+
+shuffle(player)
+print([p.name for p in player[1:]]) # print names of players
+start()
+
+for p in player[1:]:
+    p.finalScore = sum(p.scoreInRound[1:])
+
+for p in player[1:]:
+    print(f"{p.name} scored {p.finalScore} in total")
+    print()
