9e5d54ebac37bfb9749fa281716eb61ee13b5d2a
MizanZero <mizan.zafar2016@gmail.com>
2025-04-06 11:08:01 +0530

    started writing for turns

--- gambling actually improved.py
+++ gambling actually improved.py
@@ -1,5 +1,6 @@
 """
-Upto 10 players can play this game.
+Upto 10 players can play this game
+for upto 10 rounds.
 No of rounds can be decided.
 Each player has a name and a score.
 Player has a score of 0 at the start.
@@ -22,11 +23,18 @@ class Player:
         self.scoreInRound = [0 for i in range(globals().get('rounds'))]
         player.append(self)
 
-def startGame(player: list, rounds: int):
+def shuffle(player: list, rounds: int):
     players = len(player) # number of players
-    playerT = player[1:]
+    playerT = player[1:]  # temporary list of players (0th index will be None)
     random.shuffle(playerT) # shuffle the players
-    player = [None]+playerT                        # Thank you NCR
+    player = [None]+playerT # Thank you NCR
     for r in range(1,rounds+1):
         for p in range(1,players+1):
+            playTurnOf(p,r)
 
+def playTurnOf(p: Player, r: int):
+    assert r in range(1,11) , "Rounds should be between 1 and 10"
+    roll = 0
+    while choice not in ['n','y']:
+        choice = input(f"Do you want to roll? y or n: ")
+        
